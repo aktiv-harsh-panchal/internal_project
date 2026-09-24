5d7321b4b54fd720d95f827a511b56f4bbb8ddd7
aktiv-harsh-panchal <harsh.panchal@aktivsoftware.com>
2026-09-24 12:22:52 +0530

    [MOD] manifet of cad app

--- ak_solidworks/__manifest__.py
+++ ak_solidworks/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'SolidWorks CAD Integration',
-    'version': '19.0.1.0.3',
+    'version': '19.0.1.0.2',
     'summary': 'Collect data from SolidWorks CAD Server via JSON',
     'description': """
         Features:
